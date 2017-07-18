46fb2b293c7c773143f317b3c4a5654d3c95ba90
Petar Bochukov <petar.bochukov@abv.bg>
2017-07-18 16:23:42 +0300

    minor

--- routes/server.routes.js
+++ routes/server.routes.js
@@ -6,9 +6,24 @@ const attach=(app)=>{
 
     app.get("/menu", (req, res) => {
         console.log("Menu loaded");
-        res.render('all'); // relative route, no need for ./all
+        res.render('home'); // relative route, no need for ./all
        // res.render('./page.not.found')
 
+    });
+
+    app.get("/orders", (req, res) => {
+        console.log("Orders loaded");
+        res.render('orders'); 
+    });
+
+    app.get("/profile", (req, res) => {
+        console.log("Your profile");
+        res.render('profile'); 
+    });
+
+    app.get("/restaurants", (req, res) => {
+        console.log("restaurants");
+        res.render('restaurants'); 
     });
 };
 module.exports=attach;
