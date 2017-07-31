a4690caba343c27dfc71719cba8414c1ff05935c
Xenoleth <petar.yerzhabek@abv.bg>
2017-07-31 18:59:53 +0300

    removed unused code

--- app/routers/pages.router/router.js
+++ app/routers/pages.router/router.js
@@ -4,28 +4,7 @@ const { Router } = require('express');
 const attachTo = (app) => {
     const router = new Router();
     const controller = require('./controller').init();
-
-    // router
-    //     .get('/home', (req, res) => {
-    //         console.log('TEST loaded');
-    //         res.render('home');
-    //     })
-    //     .get('/menu', (req, res) => {
-    //         console.log('Menu loaded');
-    //         res.render('menu');
-    //     })
-    //     .get('/menu/forms', (req, res) => {
-    //         res.render('menu'); // relative route, no need for ./all
-    //         // res.render('./page.not.found')
-    //     })
-    //     .get('/orders', (req, res) => {
-    //         console.log('Orders loaded');
-    //         res.render('orders');
-    //     })
-    //     .get('/profile', (req, res) => {
-    //         console.log('Your profile');
-    //         res.render('user.profile.pug');
-    //     });
+    
     router
         .get('/home', (req, res) => {
             return controller.getHome(req, res);
